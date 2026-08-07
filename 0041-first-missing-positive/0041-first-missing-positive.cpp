c61476a72d99b3508bfdfb3de0bbf4dafd5f3c7e
Roshan Kumar Ray <rayroshan225@gmail.com>
2026-08-07 12:38:17 +0530

    Time: 6 ms (33.05%), Space: 55 MB (63.41%) - LeetHub

--- 0041-first-missing-positive/0041-first-missing-positive.cpp
+++ 0041-first-missing-positive/0041-first-missing-positive.cpp
@@ -1,12 +1,21 @@
 class Solution {
 public:
     int firstMissingPositive(vector<int>& nums) {
-        sort(nums.begin(), nums.end());
-        int k = 1;
-        for (int i = 0; i < nums.size(); i++) {
-            if (nums[i] == k)
-                k++;
+        int n = nums.size();
+
+        for (int i = 0; i < n; i++) {
+            while (nums[i] >= 1 &&
+                   nums[i] <= n &&
+                   nums[i] != nums[nums[i] - 1]) {
+                swap(nums[i], nums[nums[i] - 1]);
+            }
         }
-        return k;
+
+        for (int i = 0; i < n; i++) {
+            if (nums[i] != i + 1)
+                return i + 1;
+        }
+
+        return n + 1;
     }
 };
